a708e10e0f5438aa1367ad5b98b063300c6f1e9b
Maria Sidiropoulou <maroulisid@gmail.com>
2020-07-21 15:31:00 +0200

    Updates

---
+++ precache-manifest.eeb021e2f6a2b2132a9e1a34f9f43360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8a255f3a9429eda590bc6a00502927",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "95ffc396ee596efc829c",
+    "url": "/react-quiz-app/static/js/2.b6a378d6.chunk.js"
+  },
+  {
+    "revision": "a90af7845af41974c125",
+    "url": "/react-quiz-app/static/js/main.41241491.chunk.js"
+  },
+  {
+    "revision": "144806d9ebd0f357210e",
+    "url": "/react-quiz-app/static/js/runtime-main.6a615763.js"
+  },
+  {
+    "revision": "a60cf4fbd2f0a0edb2c667f682f07acb",
+    "url": "/react-quiz-app/static/media/amsterdam-silhouette.a60cf4fb.png"
+  }
+]);
